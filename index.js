fd3e0eedc686851e620961cfa5f6853281715961
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-09-16 11:36:23 +0300

    added arithmetic functions

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2 ;
+
+let random = Math.floor (Math.random () * 62) + 1;
+
+
+
+let num3 = 19; // 5*3 is 15 and 19- 15 is 4
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let max = (6,12,7,14,20);
+
+Math.max(6,12,7,14,20);
+
+console.log(Math.max(6,12,7,14,20));
+
+
+
+
+
+
+
+
+
+
+
